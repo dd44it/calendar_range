825c8fe94c81b31b5827e3518d56664b1a422fa1
dd44it <12nelson13@gmail.com>
2023-07-29 19:58:53 +0300

    feat: add dummy datarange picker

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,5 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: 'home.component.html',
 })
 export class HomeComponent {
+  disabledDates = [
+    new Date('2020-02-05'),
+    new Date('2020-02-09'),
+    new Date('2023-07-23'),
+    new Date('2023-07-24'),
+    new Date('2023-07-25'),
+  ];
+  startDate: Date = new Date();
+  endDate: Date = new Date();
 
+  onStartDateChange() {
+    // Handle start date change if needed
+  }
+
+  onEndDateChange() {
+    // Handle end date change if needed
+  }
 }
