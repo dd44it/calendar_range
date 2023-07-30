bcee1ba529b4127ef9ca338080e4a6d5c9c4d975
dd44it <12nelson13@gmail.com>
2023-07-30 14:02:34 +0300

    feat: show booked dates

--- src/app/component/booked-dates/booked-dates.component.ts
+++ src/app/component/booked-dates/booked-dates.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DateService } from 'src/app/services/date.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-booked-dates',
-  template: 'booked-dates.component.html',
+  templateUrl: 'booked-dates.component.html',
 })
-export class BookedDatesComponent {
+export class BookedDatesComponent implements OnInit, OnDestroy {
+  listBookedDates: string[] = [];
+  private getSubscriptionBookedDates?: Subscription;
+
+  constructor(
+    private datesService: DateService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getBookedDates();
+  }
+
+  getBookedDates(): void {
+    this.getSubscriptionBookedDates = this.datesService.getDates().subscribe(
+      (response) => {
+        const datesRange = this.datesService.extractIndividualDates(response);
+        this.listBookedDates = datesRange.map(date => this.datesService.formatDateToStringForClient(date));
+      },
+      (error) => {
+        console.error("Error occurred while get data to mysql. Error: ", error);
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    if (this.getSubscriptionBookedDates) {
+      this.getSubscriptionBookedDates.unsubscribe();
+    }
+  }
 
 }
