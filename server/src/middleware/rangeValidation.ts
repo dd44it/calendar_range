578fb000163a0e2ca8c921ca078ae417710c652b
dd44it <12nelson13@gmail.com>
2023-08-02 21:03:47 +0300

    refactor: upd rangeValidation

--- server/src/middleware/rangeValidation.ts
+++ server/src/middleware/rangeValidation.ts
@@ -1,18 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import db  from "../db/dbConnection";
+import { AppDataSource } from "../db/dbConnection";
 import { extractIndividualDates, isCleanDates } from "../utils/dateHelpers";
+import { Date } from "../entities/date.entity";
 
 export const rangeValidation = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const duplicateCheckQuery = "SELECT COUNT(*) AS count FROM date WHERE date_start = ? AND date_end = ?";
-    const [duplicateRows] = await db.promise().query(duplicateCheckQuery, [req.body.date_start, req.body.date_end]);
+    const dateRepository = AppDataSource.getRepository(Date);
+    const duplicateCount = await dateRepository.count({
+      where: {
+        date_start: req.body.date_start,
+        date_end: req.body.date_end,
+      },
+    });
 
-    if (duplicateRows[0].count > 0) {
+    if (duplicateCount > 0) {
       return res.status(200).json({ error: "Duplicate entry. The provided date range already exists in the database." });
     }
 
-    const [rowsBefore] = await db.promise().query("SELECT * FROM date");
-    const getDatesRange = extractIndividualDates(rowsBefore);
+    const datesBefore = await dateRepository.find();
+    const getDatesRange = extractIndividualDates(datesBefore);
     const userDateRange = extractIndividualDates([{ date_start: req.body.date_start, date_end: req.body.date_end }]);
 
     if (isCleanDates(getDatesRange, userDateRange)) {
