f8b39e5f994c4a7306e763335bd706b1f0ff38a2
dd44it <12nelson13@gmail.com>
2023-08-02 21:03:09 +0300

    upd: dateRoutes

--- server/src/routes/dateRoutes.ts
+++ server/src/routes/dateRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { getDates, sendData } from "../controllers/dateControllers";
+import { dateValidation } from "../middleware/dateValidation";
+import { rangeValidation } from "../middleware/rangeValidation";
 
 const router = express.Router();
 
 router.get("/", getDates);
-router.post("/", sendData);
+router.post("/", dateValidation, rangeValidation, sendData);
 
 export default router;
